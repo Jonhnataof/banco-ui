33fc259295a0b0f17c5d6c01abe0d1b47abb4e63
Jonhnata de Oliveira <Jonhnataof@gmail.com>
2023-04-05 22:40:51 -0300

    Adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cliente", loadChildren: () => import("./pages/cliente/cliente-listagem/cliente-listagem.module").then(m => m.ClienteListagemModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
