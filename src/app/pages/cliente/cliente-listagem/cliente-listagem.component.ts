c651ce02410f581714db3b926b63c46d9d2ce7ad
Jonhnata de Oliveira <Jonhnataof@gmail.com>
2023-04-11 22:00:54 -0300

    adicionando listagem de clientes

--- src/app/pages/cliente/cliente-listagem/cliente-listagem.component.ts
+++ src/app/pages/cliente/cliente-listagem/cliente-listagem.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteListagemService } from './cliente-listagem.service';
+import { Cliente } from 'src/app/core/entities/cliente';
 
 @Component({
   selector: 'app-cliente-listagem',
   templateUrl: './cliente-listagem.component.html',
   styleUrls: ['./cliente-listagem.component.scss']
 })
-export class ClienteListagemComponent {
+export class ClienteListagemComponent implements OnInit {
+  
+  clientes!:Cliente[];
 
+  constructor(private service: ClienteListagemService){}
+  
+  ngOnInit(){
+    this.service.getAllClientes().subscribe((clientes:Cliente[]) => {this.clientes = clientes;
+    });
+  }
 }
