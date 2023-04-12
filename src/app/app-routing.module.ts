eeff6e16aa61f5f583ebb59e1a55f9e0d114ae8f
Jonhnata de Oliveira <Jonhnataof@gmail.com>
2023-04-11 21:57:04 -0300

    Criação de rota para listagem de conta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: "cliente", loadChildren: () => import("./pages/cliente/cliente-listagem/cliente-listagem.module").then(m => m.ClienteListagemModule),
+  },
+  {
+    path: "conta", loadChildren: () => import("./pages/conta/conta-listagem/conta-listagem.module").then(m => m.ContaListagemModule),
   }
 ];
 
