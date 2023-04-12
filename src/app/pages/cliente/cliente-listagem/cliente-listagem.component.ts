d1c4383ab5db512f83bb8d5b58271e8a2ea2bb3f
Jonhnata de Oliveira <Jonhnataof@gmail.com>
2023-04-11 22:43:24 -0300

    Implementando o metódo delete

--- src/app/pages/cliente/cliente-listagem/cliente-listagem.component.ts
+++ src/app/pages/cliente/cliente-listagem/cliente-listagem.component.ts
@@ -8,13 +8,18 @@ import { Cliente } from 'src/app/core/entities/cliente';
   styleUrls: ['./cliente-listagem.component.scss']
 })
 export class ClienteListagemComponent implements OnInit {
-  
-  clientes!:Cliente[];
 
-  constructor(private service: ClienteListagemService){}
-  
-  ngOnInit(){
-    this.service.getAllClientes().subscribe((clientes:Cliente[]) => {this.clientes = clientes;
+  clientes!: Cliente[];
+
+  constructor(private service: ClienteListagemService) { }
+
+  ngOnInit() {
+    this.service.getAllClientes().subscribe((clientes: Cliente[]) => {
+      this.clientes = clientes;
     });
+  }
+
+  public confirmarDelecao(cliente: Cliente): void {
+    this.service.deleteCliente(cliente).subscribe(() => { alert('Cliente deletado com sucesso') });
   }
 }
