74308cd7b2cf19d4a7fac657c729c44339e6953e
Jonhnata de Oliveira <Jonhnataof@gmail.com>
2023-04-20 21:54:39 -0300

    Adicionando navegação para formulário

--- src/app/pages/conta/conta-listagem/conta-listagem.component.ts
+++ src/app/pages/conta/conta-listagem/conta-listagem.component.ts
@@ -1,33 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Conta } from '@entities';
 import { ContaListagemService } from './conta-listagem.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conta-listagem',
   templateUrl: './conta-listagem.component.html',
   styleUrls: ['./conta-listagem.component.scss']
 })
-export class ContaListagemComponent  implements OnInit {
+export class ContaListagemComponent implements OnInit {
   contas!: Conta[];
 
-  constructor (private service: ContaListagemService) {}
+  constructor(
+    private service: ContaListagemService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-   this.pesquisaContas();
+    this.pesquisaContas();
   }
 
   private pesquisaContas(): void {
-    this.service.getAllContas().subscribe((contas:Conta[]) => {this.contas = contas; 
+    this.service.getAllContas().subscribe((contas: Conta[]) => {
+      this.contas = contas;
     });
   }
 
   public confirmarDelecao(conta: Conta): void {
-    if(confirm("Deseja mesmo deletar a conta?")){
-      this.service.deleteConta(conta).subscribe((resp) => { 
+    if (confirm("Deseja mesmo deletar a conta?")) {
+      this.service.deleteConta(conta).subscribe((resp) => {
         console.log(resp)
         alert('Conta deletado com sucesso');
         this.pesquisaContas();
-    });
+      });
+    }
   }
-}
+
+  public navegarParaFormulario(conta?: Conta) {
+    if (conta != null){
+      this.router.navigate(['conta/formulario/', conta.id]);
+    }else{
+      this.router.navigate(['conta/formulario']);
+    }
+  }
+    
 }
